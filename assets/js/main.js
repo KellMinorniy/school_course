12ed43fe227a0b5ae0dc8ef6e610aa4d74dac5ea
Solo Coder <kisliydanecka@gmail.com>
2023-12-21 01:32:17 +0300

    debug

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 // Animate Scroll
 $(document).ready(function() {
     $('a').on('click', function(event) {
-        let linkHash = this.hash;
-        if (linkHash) {
+        let linkScroll = this.hash;
+        if (linkScroll) {
             event.preventDefault();
             $('html, body').animate({
-                scrollTop: $(linkHash).offset().top
+                scrollTop: $(linkScroll).offset().top
             }, 1000, function() {
-                window.location.hash = linkHash;
+                window.location.hash = linkScroll;
             });
         }
     });
